38b1dbb0e04deb42a55fa49a04af20752612f9b4
Rupaj Sen <rupajps4@gmail.com>
2022-02-01 23:34:41 +0530

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Rupaj Sen on 01/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
